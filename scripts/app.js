3b24fd06b0a0767880d3e79b5843d9235289337c
pain <1017202203@qq.com>
2017-07-10 17:27:59 +0800

    更新

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+/*
+app.js是整个angularjs应用程序的入口
+创建应用模块
+*/
+
+var yike = angular.module("yike",["ngRoute","Controller"]);
+// 定义 toogle方法
+// $(document).ready(function(){
+// })  == yike.run([]);
+yike.run(["$rootScope",function($rootScope){
+	// 往$rootScope上绑定toggle
+	$rootScope.collapsed = false;
+	$rootScope.toggle = function(){
+		// 找到nav节点 添加collapse类
+		$rootScope.collapsed = !$rootScope.collapsed
+		// 找到dd节点，改变transform的值
+		var dds = document.querySelectorAll(".navs dd");
+		// console.log(dds);
+		// 根据$rootScope.collapsed的值判断收起还是打开
+		// 遍历所有的dds 将其中的每一个dd的transform属性改成translate(0);
+			if($rootScope.collapsed){
+				for(var i=0;i<dds.length;i++){ 
+				dds[i].style.transform = "translate(0)"
+				//设置每个dd一次入场（动画过渡效果）
+				dds[i].style.transitionDuration = (i+1)*0.15+"s";
+				};	
+			}else{
+				for(var i=0;i<dds.length;i++){ 
+				dds[i].style.transform = "translate(-100%)"
+				dds[i].style.transitionDuration = (dds.length-i)*0.15+"s";
+				};
+			};
+	};
+}]);
+// 解决锚点乱码bug
+yike.config(function($locationProvider){
+	$locationProvider.hashPrefix("");
+});
+// 配置路由
+	yike.config(["$routeProvider",function($routeProvider){
+		$routeProvider.when("/today",{
+			templateUrl:"./views/today.html"
+		}).otherwise({
+
+		})
+	}])
---
+++ scripts/controllers.js
@@ -0,0 +1,16 @@
+// 放所有的控制器 -专门用于管理控制器
+// 将所有控制器整体定义成一个控制器模块;
+var Controller = angular.module("Controller",[]);
+// 测试；
+Controller.controller("navController",["$scope",function($scope){
+	// console.log("进入")
+	// 模拟模型层数据
+	$scope.obj = [
+	{url:'#/today',icon:'icon-home',text:"今日一刻"},
+	{url:'#/older',icon:'icon-file-empty',text:"往期内容"},
+	{url:'#/author',icon:'icon-pencil',text:"热门作者"},
+	{url:'#/category',icon:'icon-menu',text:"栏目浏览"},
+	{url:'#/favourite',icon:'icon-heart',text:"我的喜欢"},
+	{url:'#/settings',icon:'icon-cog',text:"设置"}
+	]
+}]);
