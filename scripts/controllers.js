503a7a3c316d37efb4f5685ca1d0db3322a41c09
pain <1017202203@qq.com>
2017-07-11 09:52:04 +0800

    更新

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,8 +1,9 @@
 // 放所有的控制器 -专门用于管理控制器
 // 将所有控制器整体定义成一个控制器模块;
-var Controller = angular.module("Controller",[]);
+var Controller 
 // 测试；
-Controller.controller("navController",["$scope",function($scope){
+angular.module("Controller",[])
+.controller("navController",["$scope",function($scope){
 	// console.log("进入")
 	// 模拟模型层数据
 	$scope.obj = [
@@ -13,5 +14,11 @@ Controller.controller("navController",["$scope",function($scope){
 	{url:'#/favourite',icon:'icon-heart',text:"我的喜欢"},
 	{url:'#/settings',icon:'icon-cog',text:"设置"}
 	]
-}]);
+}])
+// 今日一刻的控制器
+.controller("todayContr",["$scope","$filter",function($scope,$filter){
+	// 获取今天的日期2017-7-11
+	var today = $filter('date')(new Date,"yyyy-MM-dd"); 
+	$scope.time = today;
+}])
 
